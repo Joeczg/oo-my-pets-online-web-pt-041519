4a9b8637cc3703bd5d7fa7bb40326102f3c213bb
zhenggao cai <caizhengao1992@gmail.com>
2019-05-23 20:44:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,16 @@
 class Owner
   # code goes here
   attr_accessor :cat , :dog , :fish
+  @@all = []
   def initialize(species)
     @species = species
     @name = name 
+    @@all << self
   end
   def species
     @species = species
+  end
+  def self.all
+    @@all
   end
 end
