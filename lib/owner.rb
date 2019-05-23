41ce88e4dba68b7edb76f0e66e3d77f46ec3edf6
zhenggao cai <caizhengao1992@gmail.com>
2019-05-23 21:38:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
   # code goes here
-  attr_accessor :cat , :dog , :fish, :name, @pets
+  attr_accessor :cat , :dog , :fish, :name, :@pets
   @@all = []
   @@count = []
   @@reset_all = []
